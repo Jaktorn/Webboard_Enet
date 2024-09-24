7d4a76a273e71fc6e46eab54945c55ec3423ea32
Jaktorn <156067837+Jaktorn@users.noreply.github.com>
2024-09-24 11:52:30 +0700

    update login

--- login.php
+++ login.php
@@ -17,42 +17,41 @@ if(isset($_SESSION['id'])){
     <title>Login</title>
 </head>
 <body>
-<div class="container">
+    <div class="container-lg">
     <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
     <?php include "nav.php" ?>
-
-    <div class="row mt-4">
-        <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div> 
-        <div class="col-lg-4 col-md-6 col-sm-8 col-10">
-            <div class="card bg-light text-dark">
-                <div class="card-header">เข้าสู่ระบบ</div>
-                <div class="card-body">
-                    <form action="verify.php" method="post">
-                        <div class="form-group">
-                            <label class="form-label">Login:</label>
-                            <input type="text" name="login" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label">Password:</label>
-
-                            <div class="input-group">
-                                <input type="password" name="pwd" id="pwd" class="form-control" required>
-                            </div>
-
-                            
-                        </div>
-                        <div class="mt-3 d-flex justify-content-center">                            
-                            <button type="submit" class="btn btn-success btn-sm me-2">Login</button>
-                            <button type="reset" class="btn btn-danger btn-sm">Reset</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-sm-8 col-md-6 col-lg-4 mx-auto">
+        <div class="card bg-light text-dark">
+        <div class="card-header">
+            เข้าสู่ระบบ
         </div>
-        <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div>
+        <div class="card-body">
+            <form action="verify.php" method="post">
+                <div class="form-group">
+                    <label for="login" class="form-label">Login :</label>
+                    <input id="login" type="text" class="form-control" name="login">
+                </div>
+                <div class="form-group mt-3">
+                    <label for="pwd" class="form-label">Password :</label>
+                    <input id="pwd" type="password" class="form-control" name="pwd">
+                </div>
+                <div class="mt-3 d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success  me-2">Login</button>
+                    <button type="reset" class="btn btn-danger  ">Reset</button>
+                </div>
+            </form>
+        </div>
     </div>
- 
-    <div align="center" class="mt-3">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a></div>
     </div>
+    <
+   
+    <br>
+    <div style="text-align: center;">
+        ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a>
+    </div>
+    </div>
+    </div>
+   
 </body>
 </html>
