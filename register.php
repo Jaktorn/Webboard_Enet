eb97f8a232f9449c7dbae8340014a2c9c2187ba5
Jaktorn <156067837+Jaktorn@users.noreply.github.com>
2024-10-01 10:08:23 +0700

    update register and create register_save.php

--- register.php
+++ register.php
@@ -1,33 +1,113 @@
 <?php
 session_start();
-if(isset($_SESSION['id'])){
-    header("location:index.php");
-    die();
+if (isset($_SESSION['id'])) {
+	header("location:index.php");
+	die();
 }
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>Register</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>register</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
 </head>
+
 <body>
-	<h1 align="center">สมัครสมาชิก</h1>
-	<hr>
-	<form>
-	<table style="border: 2px solid black" align="center" width="40%">
-		<tr><td colspan="2" style="background: #6CD2FE">กรอกข้อมูล</td></tr>
-		<tr><td>ชื่อบัญชี:</td><td><input type="text" name="username"></td></tr>
-		<tr><td>รหัสผ่าน:</td><td><input type="password" name="pass2"></td></tr>
-		<tr><td>ชื่อ-นามสกุล:</td><td><input type="text" name="name_surname"></td></tr>
-		<tr><td rowspan="3">เพศ:</td><td><input type="radio" name="gender" value="male">ชาย</td></tr>
-		<tr><td><input type="radio" name="gender" value="female">หญิง</td></tr>
-		<tr><td><input type="radio" name="gender" value="other">อื่น</td></tr>
-		<tr><td>อีเมล์:</td><td><input type="text" name="email"></td></tr>
-		<tr><td colspan="2" align="center"><input type="submit" value="สมัครสมาชิก">
-			</td></tr>
-	</table>
-	</form><br>
-	<div align="center"><a href="index.php">กลับไปหน้าหลัก</a></div>	
+
+	<div class="container">
+		<h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
+		<?php
+		include("nav.php");
+		?>
+
+		<div class="row mt-4">
+			<div class="col-lg-3 col-md-2 col-sm-1"></div>
+			<div class="col-lg-6 col-md-8 col-sm-10">
+
+				<?php
+				if (isset($_SESSION['add_login'])) {
+					if ($_SESSION['add_login'] == "error") {
+						echo "<div class='alert alert-danger'>
+                                ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา
+                            </div>";
+					} else {
+						echo "<div class='alert alert-success'>
+                                เพิ่มบัญชีเรียบร้อยแล้ว
+                            </div>";
+					}
+					unset($_SESSION['add_login']);
+				}
+				?>
+
+				<div class="card border-primary">
+					<div class="card-header bg-primary text-white"> เข้าสู่ระบบ </div>
+					<div class="card-body">
+						<form action="register_save.php" method="post">
+							<div class="row">
+								<label class="col-lg-3 col-form-label" for="login"> ชื่อบัญชี: </label>
+								<div class="col-lg-9">
+									<input type="text" name="login" id="login" class="form-control" required>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label for="pwd" class="col-lg-3 col-form-label"> รหัสผ่าน: </label>
+								<div class="col-lg-9">
+									<input type="password" name="pwd" id="pwd" class="form-control" required>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label label class="col-lg-3 col-form-label" for="name"> ชื่อ-นามสกุล: </label>
+								<div class="col-lg-9">
+									<input type="text" name="name" id="name" class="form-control" required>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label class="col-lg-3 form-label"> เพศ: </label>
+								<div class="col-lg-9">
+									<div class="form-check">
+										<input type="radio" name="gender" value="m" id="m" class="form-check-input" required>
+										<label for="m" class="form-check-label">ชาย</label>
+									</div>
+									<div class="form-check">
+										<input type="radio" name="gender" value="f" id="f" class="form-check-input" required>
+										<label for="f" class="form-check-label">หญิง</label>
+									</div>
+									<div class="form-check">
+										<input type="radio" name="gender" value="o" id="o" class="form-check-input" required>
+										<label for="o" class="form-check-label">อื่นๆ</label>
+									</div>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<label for="email" class="col-lg-3 col-form-label"> อีเมล: </label>
+								<div class="col-lg-9">
+									<input type="email" name="email" id="email" class="form-control" required>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<div class="col-lg-9 d-flex justify-content-center">
+									<button type="submit" class="btn btn-primary btn-sm me-2">
+										<i class="bi bi-save2"></i> สมัครสมาชิก
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-2 col-sm-1"></div>
+		</div>
+	</div>
+	<br>
 </body>
-</html>		
+
+</html>
