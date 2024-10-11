7d7e7c33c520c2a51fd809bb11b9f60184012090
Jaktorn <156067837+Jaktorn@users.noreply.github.com>
2024-10-11 14:23:04 +0700

    create post_svae and update delete inde post

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ session_start();
 <body>
     <div class="container">
         <h1 style="text-align: center ;">Webboard Kakkak</h1>
-        <?php include "nav.php" ?>
+        <?php include "nav.php"; ?>
         <div class="mt-3 d-flex justify-content-between">
             <div>
                 <span><label>หมวดหมู่</label></span>
@@ -45,14 +45,14 @@ session_start();
                         $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
                         $sql = "SELECT * FROM category";
                         foreach ($conn->query($sql) as $row) {
-                            echo "<li><a class='dropdown-item' href=#>$row[name]</a></li>";
+                            echo "<li><a class='dropdown-item' href='#'>$row[name]</a></li>";
                         }
                         $conn = null;
                         ?>
-
                     </ul>
                 </span>
-            </div><?php if (isset($_SESSION['id'])) { ?>
+            </div>
+            <?php if (isset($_SESSION['id'])) { ?>
                 <div><a href="newpost.php" class="btn btn-success btn-sm">
                         <i class="bi bi-plus"></i> สร้างกระทู้ใหม่</a></div>
             <?php } ?>
@@ -60,20 +60,22 @@ session_start();
         <table class="table table-striped mt-4 ">
             <?php
             $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
-            $sql = "SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1 
+            $sql = "SELECT t3.name, t1.title, t1.id, t2.login, t1.post_date FROM post as t1 
                     INNER JOIN user as t2 ON (t1.user_id=t2.id)
                     INNER JOIN category as t3 ON (t1.cat_id = t3.id) ORDER BY t1.post_date DESC";
             $result = $conn->query($sql);
             while ($row = $result->fetch()) {
                 echo "<tr><td> $row[0]  <a href=post.php?id=$row[2] style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4]</td>";
-                
+                // เพิ่มปุ่มลบโพสต์
+                if (isset($_SESSION['id']) && $_SESSION['role'] == 'a') {
+                    echo "<td><a href='delete.php?id=$row[2]' class='btn btn-danger btn-sm' onclick='return myFunction();'><i class='bi bi-trash'></i></a></td>";
+                }
+                echo "</tr>";
             }
-            $conn=null;
+            $conn = null;
             ?>
-
         </table>
         <ul>
-
         </ul>
     </div>
 </body>
